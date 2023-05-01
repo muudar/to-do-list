6881221b8dce0d125828fb56159497d5a0af9942
Mudhar <mohammedmudher@hotmail.com>
2023-05-02 01:17:49 +0300

    added fixing if local storage is empty

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import {loadProjects} from "./loaders";
 
 
 let projects = getProjects();
+if(!projects){
+    let projectsArr = [];
+    projectsArr.push(new Project("My Project"));
+    localStorage.setItem("projects", projectsArr);
+}
 
 loadProjects(projects, 0);
 
